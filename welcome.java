d39d8ae455f13f73537db0e8321d1c489e852045
thawinner <tha.winner.dev@gmail.com>
2026-03-23 10:35:47 +0700

    Add welcome class with main menu options

---
+++ welcome.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class welcome {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("\n------------- Welcome to I3 Typing Master v2026 -------------");
+            System.out.println("To Begin, please select one of the following options:");
+            System.out.println("1. User registering");
+            System.out.println("2. User reset password");
+            System.out.println("3. User login");
+            System.out.println("4. List of test results for a user");
+            System.out.println("5. User typing test");
+            System.out.println("6. About Us");
+            System.out.println("7. Exit");
+            System.out.print("Choose an option: - ");
+
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+
+
+            }
+        } while (choice != 7);
+        sc.close();
+    }
+}
